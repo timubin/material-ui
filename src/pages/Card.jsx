0754b1d234235e363f4aaffc59ed48d40a0c26a2
timubin <71239469+timubin@users.noreply.github.com>
2022-01-06 12:37:17 +0600

    some update

--- src/pages/Card.jsx
+++ src/pages/Card.jsx
@@ -1,12 +1,35 @@
-import { Card, CardHeader, IconButton } from "@material-ui/core";
-import { DeleteOutline } from "@material-ui/icons";
-import React from "react";
-
+import React, { useState } from "react";
+import { Button } from "@material-ui/core";
+import "./card.css";
 const Cards = () => {
+  const [count, setCount] = useState(1);
+  const increment = () => {
+    setCount(count + 1);
+    if (count === 10) {
+      alert("not Incerment");
+      setCount(10);
+    }
+  };
+  const diccrement = () => {
+    setCount(count - 1);
+
+    if (count === 0) {
+      alert("not diccrement");
+      setCount(0);
+    }
+  };
   return (
-    <div>
-      <h1>My name is tarikul islam mubin</h1>
-    </div>
+    <>
+      <Button variant="contained" onClick={() => increment()}>
+        +
+      </Button>
+
+      <h1>{count}</h1>
+      <p>{count}@</p>
+      <Button variant="contained" onClick={() => diccrement()}>
+        -
+      </Button>
+    </>
   );
 };
 
